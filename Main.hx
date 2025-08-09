cea64818157218c5032fff602b45ccf91afb4e4b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-08 22:47:11 -0500

    using lime library

--- Main.hx
+++ Main.hx
@@ -1,3 +1,11 @@
 package;
 
-class Main {}
+import comma.CliApp;
+
+class Main {
+	static function main() {
+		// pass third parameter true for apps with single command
+		var app = new CliApp("TestCliApp", "0.1.0");
+		app.start();
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Sinco Command Line
 
 ## Libraries
+- [`lime`](https://github.com/openfl/lime) by openfl
 - [`comma`](https://github.com/metincetin/comma/tree/main) by metincetin
